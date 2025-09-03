1bb28c88b4b4e63b48805b18ac2f4432e034ceaa
SPYop999 <karthikcruz24@gmail.com>
2025-09-03 23:10:57 +0530

    fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // ✅ important for Netlify
-})
+  base: "./",   // 👈 add this line
+});
+
